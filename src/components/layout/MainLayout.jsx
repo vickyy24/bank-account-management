54b9137d5f618525c530fdbe1f6b324830bbde0e
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-08-19 12:15:57 +0530

    Create main layout

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+
+const MainLayout=({children})=>{
+
+    const [isSidebarOpen,setIsSidebarOpen] = useState(false)
+
+    const handleOpenSidebar=()=>{
+        setIsSidebarOpen(true)
+    }
+
+    const handleCloseSidebar=()=>{
+        setIsSidebarOpen(false)
+    }
+
+    return(
+
+        <div className="min-h-screen bg-gray-50">
+
+            <div className="flex min-h-screen">
+
+                <Sidebar isOpen={isSidebarOpen} onClose={handleCloseSidebar}/>
+
+                <div className="flex-1 min-w-0">
+
+                    <Navbar onMenuClick={handleOpenSidebar}/>
+
+                    <main className="p-4 sm:p-6 lg:p-8">
+                        {children}
+                    </main>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default MainLayout
