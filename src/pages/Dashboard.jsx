af759123dc18ef18a9e1af1e6a8afe7c81c87583
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-08-19 12:17:37 +0530

    Create dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+const Dashboard=()=>{
+
+    return(
+
+        <div>
+
+            <h1 className="text-2xl font-semibold text-gray-800">
+                Dashboard
+            </h1>
+
+            <p className="mt-1 text-gray-600">
+                Bank account management overview
+            </p>
+
+        </div>
+
+    )
+}
+
+export default Dashboard
